97f6e6441462126389f6bcd38a48105819445643
Kosench <ilyakosenko09@gmail.com>
2026-03-22 21:53:03 +1000

    feat: add RequestID, Logger and Panic recovery middleware

---
+++ internal/core/transport/http/middleware/common.go
@@ -0,0 +1,65 @@
+package core_http_middleware
+
+import (
+	"context"
+	"net/http"
+
+	core_logger "github.com/Kosench/golang-todoapp/internal/core/logger"
+	core_http_response "github.com/Kosench/golang-todoapp/internal/core/transport/http/response"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+const (
+	requestIDHeader = "X-Request-ID"
+)
+
+func RequestID() Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := r.Header.Get(requestIDHeader)
+			if requestID == "" {
+				requestID = uuid.NewString()
+			}
+
+			r.Header.Set(requestIDHeader, requestID)
+			w.Header().Set(requestIDHeader, requestID)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func Logger(log *core_logger.Logger) Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestID := r.Header.Get(requestIDHeader)
+
+			l := log.With(
+				zap.String("request_id", requestID),
+				zap.String("url", r.URL.String()),
+			)
+
+			ctx := context.WithValue(r.Context(), "log", l)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+func Panic() Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			log := core_logger.FromContext(ctx)
+			responseHandler := core_http_response.NewHTTPResponseHandler(log, w)
+			defer func() {
+				if p := recover(); p != nil {
+					responseHandler.PanicResponse(p, "during handler http request got unexpected panic")
+				}
+			}()
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
