0c1d9433f2c8c320827ace3307b843dc82fca907
Kosench <ilyakosenko09@gmail.com>
2026-03-27 22:39:50 +1000

    refactor(users/http): move pagination validation to http layer

--- internal/features/users/service/get_users.go
+++ internal/features/users/service/get_users.go
@@ -5,24 +5,9 @@ import (
 	"fmt"
 
 	"github.com/Kosench/golang-todoapp/internal/core/domain"
-	core_errors "github.com/Kosench/golang-todoapp/internal/core/errors"
 )
 
 func (s *UserService) GetUsers(ctx context.Context, limit *int, offset *int) ([]domain.User, error) {
-	if limit != nil && *limit < 0 {
-		return nil, fmt.Errorf(
-			"limit must be non-negative: %w",
-			core_errors.ErrInvalidArgument,
-		)
-	}
-
-	if offset != nil && *offset < 0 {
-		return nil, fmt.Errorf(
-			"offset must be non-negative: %w",
-			core_errors.ErrInvalidArgument,
-		)
-	}
-
 	users, err := s.usersRepository.GetUsers(ctx, limit, offset)
 	if err != nil {
 		return nil, fmt.Errorf("get users from repository: %w", err)
